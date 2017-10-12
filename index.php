8a810903d6497ae350a47d66cca4d74587fc846c
Rob Tulloch <rob.tulloch@gmail.com>
2017-10-12 16:38:37 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<HTML>
+<HEAD>
+   <TITLE></TITLE>
+   <META NAME="Author" CONTENT="Rob Tulloch">
+   <META NAME="GENERATOR" CONTENT="Text Editor">
+</HEAD>
+<BODY>
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/analyticstracking.php";
+   include_once($path);
+?>
+<H2 ALIGN=CENTER>Through the Looking Glass and What Alice Found There</H2>
+
+<CENTER><P><BLINK><FONT COLOR="#FF0000" SIZE=+1><B>Warning:</B></FONT></BLINK> High Multimedia Content!!!!!!!</P></CENTER>
+
+<center><p>For a less intense experince click <a href="textch7.php">here</a>. (The only difference is that it's faster.)</p></center>
+
+<CENTER><P>Send any comments to me at <A HREF="mailto:webmaster@lists.syrrys.com">Rob.Tulloch@sk.sympatico.ca</A></P></CENTER>
+
+<CENTER><P><A HREF="glassch7.php"><IMG SRC="images/anim1.gif" ALT="Looking Glass Image" HEIGHT=400 WIDTH=400></A></P></CENTER>
+
+<CENTER><P>
+<HR WIDTH="100%"></P></CENTER>
+
+</BODY>
+</HTML>
